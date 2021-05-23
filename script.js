ee06784b57068848822f83e5b1f46ccbb2f6eb73
Minh Le <lenguyetminh1998@gmail.com>
2021-05-23 19:59:26 -0400

    DONE

--- script.js
+++ script.js
@@ -9,10 +9,15 @@ var app = new Vue({
       h: false, i: false, j: false, k: false, l: false, m: false, n: false,
       o: false, p: false, q: false, r: false, s: false, t: false, u: false, 
       v: false, w: false, x: false, y: false, z: false, 
-      aa: false, ab: false, ac: false, ad: false, ae: false, af: false, ag: false, 
+      aa: false, ab: false, ac: false, ad: false, ad2: false, ae: false, af: false, ag: false, 
       ah: false, ai: false, aj: false, ak: false, al: false, am: false, an: false,
       ao: false, ap: false, aq: false, ar: false, as: false, at: false, au: false, 
       av: false, aw: false, ax: false, ay: false, az: false, 
+
+      ba: false, bb: false, bc: false, bd: false, be: false, bf: false, bg: false, 
+      bh: false, bi: false, bj: false, bk: false, bl: false, bm: false, bn: false,
+      bo: false, bp: false, bq: false, br: false, bs: false, bt: false, bu: false, 
+      bv: false, bw: false, bx: false, by: false, bz: false, 
 
      
 
