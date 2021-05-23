70e424f2286c79e432e853b05231a2d105427994
Minh Le <lenguyetminh1998@gmail.com>
2021-05-23 15:36:36 -0400

    Ugh

--- script.js
+++ script.js
@@ -8,7 +8,14 @@ var app = new Vue({
       a: false, b: false, c: false, d: false, e: false, f: false, g: false, 
       h: false, i: false, j: false, k: false, l: false, m: false, n: false,
       o: false, p: false, q: false, r: false, s: false, t: false, u: false, 
-      v: false, w: false, x: false, y: false, z: false, aa: false, ab: false, 
+      v: false, w: false, x: false, y: false, z: false, 
+      aa: false, ab: false, ac: false, ad: false, ae: false, af: false, ag: false, 
+      ah: false, ai: false, aj: false, ak: false, al: false, am: false, an: false,
+      ao: false, ap: false, aq: false, ar: false, as: false, at: false, au: false, 
+      av: false, aw: false, ax: false, ay: false, az: false, 
+
+     
+
     },
     methods: {
       humanizeURL: function (url) {
